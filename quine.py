e56d434721fe2cda6342b2b1e218da03fb363d68
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-04-20 15:47:40 +0200

    Implemented all necessary functions, some corner cases still not handled

--- quine.py
+++ quine.py
@@ -1,6 +1,9 @@
 import string
 import re
 from collections import defaultdict
+from itertools import combinations
+
+
 
 # Checks if given list of tokens represents syntactically correct logical expression
 def check_syntax(expression_tokens):
@@ -150,11 +153,23 @@ def evaluate(rpn_expression, values):
         elif token == '~':
             stack = stack[:-1] + [ not stack[-1] ]
 
+        elif token == '1':
+            stack += [True]
+
+        elif token == '0':
+            stack += [False]
+
         else:
             stack += [ values[token] ]
 
 
     return stack[0]
+
+
+
+# Extracts all variables from expression and creates sorted list of them
+def get_variables(rpn_expression):
+    return sorted(set([token for token in rpn_expression if token not in '&|^>=~01']))
 
 
 
@@ -168,7 +183,7 @@ def get_bitmask(number, width):
 def get_values(bitmask_number, variables):
 
     result = {}
-    values = map(lambda x : x == '1', get_bitmask(bitmask_number, len(variables)))
+    values = map(lambda x: x == '1', get_bitmask(bitmask_number, len(variables)))
 
     for variable, value in zip(variables, values):
         result[variable] = value
@@ -184,18 +199,125 @@ def get_ones_number(bitmask_number):
 
 
 # Finds all value sets for wchich the expression evaluetes to True
-def find_all_minterms(rpn_expression):
+def get_minterms(rpn_expression):
 
-    variables = set([token for token in rpn_expression if token not in '&|^>=~'])
-    upper_bound = 1 << len(variables)
+    variables = get_variables(rpn_expression)
+    set_size = len(variables)
+    upper_bound = 1 << set_size
 
-    minterms = defaultdict(lambda: [])
+    minterms = set()
 
     for bitmask_number in range(upper_bound):
 
         values = get_values(bitmask_number, variables)
 
         if evaluate(rpn_expression, values) == True:
-            minterms[get_ones_number(bitmask_number)] += [values]
+            minterm_bitmask = 1 << bitmask_number
+            minterms.add((minterm_bitmask, get_bitmask(bitmask_number, set_size)))
 
-    return minterms
+    return minterms, variables
+
+
+
+# Merges two implicants (e.g. merge('100', '110') -> '1-0')
+def merge_implicants(imp_a, imp_b):
+
+    mask_a = imp_a[1]
+    mask_b = imp_b[1]
+
+    if len([_ for bit_a, bit_b in zip(mask_a, mask_b) if bit_a != bit_b]) != 1:
+        return None
+
+    result_mask = ""
+
+    for bit_a, bit_b in zip(mask_a, mask_b):
+        result_mask += bit_a if bit_a == bit_b else '-'
+
+    return (imp_a[0] | imp_b[0]), result_mask
+
+
+
+# Groups implicants until no other combination is possible
+def get_prime_implicants(minterms):
+
+    implicants = minterms
+    prime_implicants = set()
+
+    while True:
+
+        used_implicants = set()
+        new_implicants = set()
+
+        for imp_a, imp_b in combinations(implicants, 2):
+
+            merged = merge_implicants(imp_a, imp_b)
+
+            if merged is not None:
+                new_implicants.add(merged)
+                used_implicants.add(imp_a[0])
+                used_implicants.add(imp_b[0])
+
+        for imp in implicants:
+            if imp[0] not in used_implicants:
+                prime_implicants.add(imp)
+
+        implicants = new_implicants
+
+        if len(implicants) == 0: # No other merge is possible
+            break
+
+    return prime_implicants
+
+
+
+# Fids the smallest set of prime implicants that covers all minterms
+def get_minterms_cover(minterms, prime_implicants):
+
+    minterms_mask = 0
+    for minterm, _ in minterms:
+        minterms_mask |= minterm
+
+    for i in range(1, len(minterms)+1):
+        for primes_set in combinations(prime_implicants, i):
+
+            primes_mask = 0
+            for prime, _ in primes_set:
+                primes_mask |= prime
+
+            if primes_mask == minterms_mask:
+                return primes_set
+
+    return None
+
+
+
+# Converts implicant into variables conjunction string
+def implicant_to_string(implicant, variables):
+
+    result = []
+    for token, variable in zip(implicant[1], variables):
+        if token == '1':
+            result += [variable]
+        elif token == '0':
+            result += ['~' + variable]
+
+    return ' & '.join(result)
+
+
+
+# Converts set of implicants into logical expression string
+def implicants_set_to_string(cover, variables):
+
+    result = [implicant_to_string(implicant, variables) for implicant in cover]
+    return ' | '.join(result)
+
+
+
+# Simplifies logical expression with Quine-McCluskey algorithm
+def simplify(expression):
+
+    rpn_expression = to_rpn(expression)
+    minterms, variables = get_minterms(rpn_expression)
+    prime_implicants = get_prime_implicants(minterms)
+    minterms_cover = get_minterms_cover(minterms, prime_implicants)
+    return implicants_set_to_string(minterms_cover, variables)
